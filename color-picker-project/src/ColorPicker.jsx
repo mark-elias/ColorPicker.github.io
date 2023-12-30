4df70c91eca68b206b03abd01af638f4279d6215
mark-elias <mark7elias@gmail.com>
2023-12-30 15:57:47 -0800

    updated styling

--- color-picker-project/src/ColorPicker.jsx
+++ color-picker-project/src/ColorPicker.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+
 function ColorPicker() {
+    const [color, setColor] = useState("#2FF109");
+
+    function updateColor(event){
+        setColor(event.target.value)
+    }
+
+    
     return(
-        <div >
-            <p>hello world</p>
-            <h1>hello world</h1>
-        </div>
+        <>
+            <div className="color-picker-container">
+                <h1>Color Picker</h1>
+                <div className="color-display" style={{backgroundColor: color}}>
+                    <p>Selected Color: {color}</p>
+                </div>
+                <label>Select a Color</label>
+                <input type="color" value={color} onChange={updateColor}></input>
+            </div>
+        </>
     );
 }
 
